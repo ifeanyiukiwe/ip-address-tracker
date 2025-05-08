6f4f9c02a7dcdabd809f0db541f1d1a8a3eaf53e
Ifeanyi Ukiwe <135234774+ifeanyiukiwe@users.noreply.github.com>
2025-05-08 20:02:36 +0100

    Adjusting the font

--- src/components/IpData.jsx
+++ src/components/IpData.jsx
@@ -14,12 +14,12 @@ const IPData = () => {
     <div className="absolute top-[180px] left-1/2 transform -translate-x-1/2 w-[90%] max-w-5xl bg-white shadow-md p-6 rounded-lg flex flex-col md:flex-row md:justify-between md:items-center text-center z-30">
       <div className="flex-1 px-4 sm:border-r-2 border-gray-200 mb-4 md:mb-0">
         <strong className="block text-sm text-gray-500 mb-1">IP Address</strong>
-        <p className="text-lg font-semibold">{ip}</p>
+        <p className="text-sm font-semibold">{ip}</p>
       </div>
 
       <div className="flex-1 px-4 sm:border-r-2 border-gray-200 mb-4 md:mb-0">
         <strong className="block text-sm text-gray-500 mb-1">Location</strong>
-        <p className="text-lg font-semibold">
+        <p className="text-sm font-semibold">
           {location.city}, {location.region}, {location.postalCode},{" "}
           {location.country}
         </p>
@@ -27,12 +27,12 @@ const IPData = () => {
 
       <div className="flex-1 px-4 sm:border-r-2 border-gray-200 mb-4 md:mb-0">
         <strong className="block text-sm text-gray-500 mb-1">Timezone</strong>
-        <p className="text-lg font-semibold">UTC {location.timezone}</p>
+        <p className="text-sm font-semibold">UTC {location.timezone}</p>
       </div>
 
       <div className="flex-1 px-4">
         <strong className="block text-sm text-gray-500 mb-1">ISP</strong>
-        <p className="text-lg font-semibold">{isp}</p>
+        <p className="text-sm font-semibold">{isp}</p>
       </div>
     </div>
   );
